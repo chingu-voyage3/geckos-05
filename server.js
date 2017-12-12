d891db5a523d1d27c810f1dccef2a48f0bb1bbe9
ckingbailey <ckingbailey@gmail.com>
2017-12-11 16:53:46 -0800

    express setup

---
+++ server.js
@@ -0,0 +1,48 @@
+"use strict"
+
+// dependencies
+const express = require("express");
+
+// env vars
+const PORT = process.env.API_PORT || 3001;
+
+// modules of dependencies
+const app = express();
+const router = express.Router();
+
+// dummy db results for testing
+const fake_projects = [
+  { name: "My Project",
+    description: "We made a thing!",
+    team: "Squids-23",
+    cohort: "Summer 2017",
+    stack: ["React", "JQuery"],
+    categories: ["landing page", "original"],
+    uniqueID: "1111111aaaaa" },
+  { name: "My Project 2",
+    description: "We made another thing!",
+    team: "Nutrias-66",
+    cohort: "Fall 2017",
+    stack: ["Vue", "D3"],
+    categories: ["Chrome extension", "Momentum clone", "original"],
+    uniqueID: "1111112bbbbb" },
+  { name: "My Project III",
+    description: "We made a third thing!",
+    team: "Pigeons-54",
+    cohort: "Winter 2017",
+    stack: ["Angular", "Express", "PostgreSQL"],
+    categories: ["full-stack", "bot", "Slack bot", "clone"],
+    uniqueID: "1111113ccccc" },
+]
+
+router.route("/projects")
+  .get((req, res) => {
+    console.log("You've reached the '/projects' route. Please leave a message and we'll call you back");
+    res.json({ fake_projects })
+  })
+
+app.use("/api", router);
+
+app.listen(PORT, () => {
+  console.log(`listening on port ${PORT}`)
+})
