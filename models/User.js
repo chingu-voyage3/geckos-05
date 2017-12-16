88ef601a8c94dcc649102105ae3a373f716a1906
Egan <ebisma@ucsc.edu>
2017-12-16 10:05:09 -0800

    Started working on user model.

---
+++ models/User.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const projectIdea = require("./projectIdea");
+
+const userSchema = new Schema({
+    name: {
+        type: String,
+        validate: {
+            validator: (name) => {
+                return name.length > 4
+            },
+            message: "Name must be longer than 4 characters"
+        },
+        required: [true, "Name is required"]
+    },
+    projectIdea: [{
+        types: Schema.Types.ObjectId,
+        ref: 'projectIdea'
+    }]
+});
+
+userSchema.virtual("numOfProjects").get(function () {
+    return this.projectIdea.length;
+});
+
+//represetsnts entire collection of data
+const User = mongoose.model('user', userSchema);
+
+module.exports = User;
