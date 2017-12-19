6eb88b5a372eaa07c46e577d9a3a13dc51e46371
ckingbailey <ckingbailey@gmail.com>
2017-12-18 16:25:41 -0800

    remove leading / in path to react

--- server.js
+++ server.js
@@ -23,19 +23,23 @@ const router = express.Router();
 // allow any origin for any route
 // TODO: I think I could change this is something more specific
 app.use("/", (req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.set("Access-Control-Allow-Origin", "*");
   next();
 })
 
 // tutorial says "Priority serve any static files"
 // does this not do the same as get("*", callback => sendFile("<react-stuff>"))?
-app.use(express.static(path.resolve(__dirname, "/react-ui/build")));
+app.use(express.static(path.resolve(__dirname, "react-ui/build")));
+
+router.get("/", (req, res) => {
+  res.send("api initialized")
+})
 
 // CRUD methods for projects api
 router.route("/projects")
   .get((req, res) => {
-    res.setHeader("Content-Type", "application/json");
-    res.send({ projects: DATA });
+    res.set("Content-Type", "application/json");
+    res.send({ "projects": DATA });
   })
 
 // use above router config for calls to /api
@@ -44,9 +48,9 @@ app.use("/api", router);
 // All remaining unhandled requests return the React app
 // TODO: put error handling in here so I know where something's gone wrong when it goes wrong
 app.get("*", (req, res) => {
-  const pathToReact = path.resolve(__dirname, "/react-ui/build", "index.html");
+  const pathToReact = path.resolve(__dirname, "react-ui/build", "index.html");
   console.log(pathToReact);
-  res.sendFile(path.resolve(pathToReact);
+  res.sendFile(path.resolve(pathToReact));
 })
 
 app.listen(PORT, () => {
