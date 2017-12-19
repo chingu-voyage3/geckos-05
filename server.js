57fe396c2ec2ff8216eee101ffae1c90d54163f8
ckingbailey <ckingbailey@gmail.com>
2017-12-18 16:35:17 -0800

    route to react from / instead of *

--- server.js
+++ server.js
@@ -47,7 +47,8 @@ app.use("/api", router);
 
 // All remaining unhandled requests return the React app
 // TODO: put error handling in here so I know where something's gone wrong when it goes wrong
-app.get("*", (req, res) => {
+// BUG: if "/" serves all sub-paths, how to restrict sub-paths and serve _only_ route "/"
+app.get("/", (req, res) => {
   const pathToReact = path.resolve(__dirname, "react-ui/build", "index.html");
   console.log(pathToReact);
   res.sendFile(path.resolve(pathToReact));
