62c9e2d09e8ebe6400044578d9ae33e22f7a7c4a
ckingbailey <ckingbailey@gmail.com>
2018-01-10 20:55:18 -0800

    vertical-align nav elements

--- react-ui/src/page_nav.js
+++ react-ui/src/page_nav.js
@@ -35,7 +35,7 @@ export default class PageNav extends Component {
           className="btn nav-btn prev-btn"
           onClick={ () => this.prevPage(this.props.current, this.props.gotoPage) }
         >
-          <svg version="1.2" baseProfile="tiny" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M14.414 5.586c-.78-.781-2.048-.781-2.828 0l-6.415 6.414 6.415 6.414c.39.391.902.586 1.414.586s1.024-.195 1.414-.586c.781-.781.781-2.047 0-2.828l-3.585-3.586 3.585-3.586c.781-.781.781-2.047 0-2.828z"/></svg>
+          <svg className="nav-arrow left-arrow" version="1.2" baseProfile="tiny" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M14.414 5.586c-.78-.781-2.048-.781-2.828 0l-6.415 6.414 6.415 6.414c.39.391.902.586 1.414.586s1.024-.195 1.414-.586c.781-.781.781-2.047 0-2.828l-3.585-3.586 3.585-3.586c.781-.781.781-2.047 0-2.828z"/></svg>
         </button>
         <ul className="nav-list">
           { this.renderNavList(this.props.current) }
@@ -44,7 +44,7 @@ export default class PageNav extends Component {
           className="btn nav-btn next-btn"
           onClick={ () => this.nextPage(this.props.current, this.props.gotoPage) }
         >
-          <svg version="1.2" baseProfile="tiny" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M8.586 5.586c-.781.781-.781 2.047 0 2.828l3.585 3.586-3.585 3.586c-.781.781-.781 2.047 0 2.828.39.391.902.586 1.414.586s1.024-.195 1.414-.586l6.415-6.414-6.415-6.414c-.78-.781-2.048-.781-2.828 0z"/></svg>
+          <svg className="nav-arrow right-arrow" version="1.2" baseProfile="tiny" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M8.586 5.586c-.781.781-.781 2.047 0 2.828l3.585 3.586-3.585 3.586c-.781.781-.781 2.047 0 2.828.39.391.902.586 1.414.586s1.024-.195 1.414-.586l6.415-6.414-6.415-6.414c-.78-.781-2.048-.781-2.828 0z"/></svg>
         </button>
       </div>
     )
