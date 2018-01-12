1ddbaf6bcf5f172e381837a301fda0a5b22d04fd
ckingbailey <ckingbailey@gmail.com>
2018-01-12 15:19:54 -0800

    style project pop up

--- react-ui/src/project.js
+++ react-ui/src/project.js
@@ -16,12 +16,12 @@ export default class ProjectPopUp extends Component {
           <button className="btn close-btn close-project-btn" onClick={ this.props.toggleShowProject }>&times;</button>
         </h2>
         <div className="project-content">
-          <img src={ this.props.picture } alt="." />
-          <p>{ this.props.description }</p>
+          <img className="project-img" src={ this.props.picture } alt="." />
+          <p className="project-description">{ this.props.description }</p>
           <section className="resource-links">{ this.props.links }</section>
-          <p><a href={this.props.url}>Github Link</a></p>
-          <p><a href={this.props.homepage}>Live Link</a></p>
-          <p>{ this.props.contributors.join(", ") }</p>
+          <p><a className="project-link repo-link" href={this.props.url}>{ this.props.url }</a></p>
+          { this.props.homepage && <p><a className="project-link demo-link" href={this.props.homepage}>{ this.props.homepage }</a></p>}
+          <p className="contributors-list">{ this.props.contributors.join(", ") }</p>
         </div>
       </div>
     )
