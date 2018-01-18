d808e7f3aa181c358ab690599e5f7324b7fa7d92
ckingbailey <ckingbailey@gmail.com>
2018-01-17 18:28:15 -0800

    resolve merge conflict and fix search-field width

--- react-ui/src/search_bar.js
+++ react-ui/src/search_bar.js
@@ -10,66 +10,39 @@ class SearchBar extends Component {
     this.props.selectValueChange(e.target.value);
   }
 
-<<<<<<< HEAD
-    render() {
-        const term = this.state.term;
-        return (
-            <div className="search-bar">
-                <form id="filterForm" name="filterForm">
-                    <input
-                      id="filterTextInput"
-                      name="filterTextInput"
-                      className="text-field search-field"
-                      type="text"
-                      placeholder="Project search"
-                      value={term}
-                      onChange={this.onInputChange.bind(this)}
-                    />
-                    <select id="filterOptions"
-                            name="filterOptions"
-                            className="dropdown"
-                            onChange={this.handleChange}>
-                        <option value="name">Project/ Repo Name</option>
-                        <option value="description">Description</option>
-                    </select>
-                </form>
-            </div>
-        )
-    }
-=======
   render() {
-      const term = this.props.term;
-      return (
-        <div className="search-bar">
-          <h3
-            className={ this.props.term ? "fade-in filter-heading" : "fade-in filter-heading hidden" }
-          >You are searching for: <span className="filter-term">{this.props.term}</span></h3>
-          <form
-            id="filterForm"
-            name="filterForm"
-            className="filter-form"
-            onSubmit={ e => e.preventDefault() }
-          >
-            <input
-              id="filterTextInput"
-              name="filterTextInput"
-              className="text-field search-field"
-              type="text"
-              placeholder="Search projects"
-              value={term}
-              onChange={this.onInputChange}
-            />
-            <select id="filterOptions"
-                    name="filterOptions"
-                    onChange={this.selectValueChange}>
-              <option value="name">Project/Repo Name</option>
-              <option value="description">Description</option>
-            </select>
-          </form>
-        </div>
-      )
+    const term = this.props.term;
+    return (
+      <div className="search-bar">
+        <h3
+          className={ this.props.term ? "fade-in filter-heading" : "fade-in filter-heading hidden" }
+        >You are searching for: <span className="filter-term">{this.props.term}</span></h3>
+        <form
+          id="filterForm"
+          name="filterForm"
+          className="filter-form"
+          onSubmit={ e => e.preventDefault() }
+        >
+          <input
+            id="filterTextInput"
+            name="filterTextInput"
+            className="text-field search-field"
+            type="text"
+            placeholder="Search projects"
+            value={term}
+            onChange={this.onInputChange}
+          />
+          <select id="filterOptions"
+                  name="filterOptions"
+                  className="dropdown"
+                  onChange={this.selectValueChange}>
+            <option value="name">Project/Repo Name</option>
+            <option value="description">Description</option>
+          </select>
+        </form>
+      </div>
+    )
   }
->>>>>>> if no projects, print "no projects found"
 }
 
 export default SearchBar;
